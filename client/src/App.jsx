5b575347490ccd2b393e1c8c02bcf56cdd5d5182
congprodh <congprodh@gmail.com>
2023-02-23 22:50:58 +0700

    feat:login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,23 @@ import Layout from './Layout'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import { UserContextProvider } from './UserContext'
+
+axios.defaults.baseURL = 'http://127.0.0.1:3000'
+axios.defaults.withCredentials = true
 
 function App() {
 
-  axios.defaults.baseURL = 'http://localhost:3000'
-
-
   return (
-    <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<IndexPage />} />
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/register' element={<RegisterPage/>} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -4,37 +4,78 @@ const { default: mongoose } = require('mongoose')
 require('dotenv').config()
 const app = express()
 const User = require('./models/User')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+const cookieParser = require('cookie-parser')
 
 const bcryptSalt = bcrypt.genSaltSync(12);
+const jwtSecret = 'fhniakjw3hri2ujeksadbj123nojdank'
 
 app.use(express.json())
 
+app.use(cookieParser())
+
 app.use(cors({
-    credentials:true,
-    origin:'http://localhost:5173'
+    credentials: true,
+    origin: 'http://127.0.0.1:5173'
 }))
 
 mongoose.set('strictQuery', true)
-mongoose.connect(process.env.MONGO_URL) 
+mongoose.connect(process.env.MONGO_URL)
 
-app.get('/test', (req, res)=>{
+app.get('/test', (req, res) => {
     res.json('test ok')
 })
 
-app.post('/register', async(req,res) => {
-    const {name, email, password} = req.body
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body
     try {
-       const userDoc = await User.create({
+        const userDoc = await User.create({
             name,
             email,
-            password:bcrypt.hashSync(password, bcryptSalt)
+            password: bcrypt.hashSync(password, bcryptSalt)
         })
-        res.json(userDoc) 
+        res.json(userDoc)
     } catch (error) {
         res.status(422).json(e)
     }
-    
+
+})
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body
+    const userDoc = await User.findOne({ email })
+    if (userDoc) {
+
+        const passOk = bcrypt.compareSync(password, userDoc.password)
+        if (passOk) {
+            jwt.sign({
+                email: userDoc.email,
+                id: userDoc._id,
+            }, jwtSecret, {}, (err, token) => {
+                if (err) throw err
+                res.cookie('token', token).json(userDoc)
+            })
+        }
+        else {
+            res.status(422).json('pass not ok')
+        }
+    } else {
+        res.json('Not found')
+    }
+})
+
+
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err
+      const {name,email,_id} = await User.findById(userData.id)
+      res.json({name,email,_id})
+    })
+  } else {
+    res.json(null)
+  }
 })
 
 app.listen(3000)
