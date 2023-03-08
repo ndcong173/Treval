8f385643a9e8f1639f5775f7f2073b5a50c98bff
congprodh <congprodh@gmail.com>
2023-03-08 23:33:49 +0700

    feat: index page interface

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,10 +1,28 @@
-import { Link } from "react-router-dom";
-import Header from "../Header";
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 export default function IndexPage(){
+
+  const [places, setPlaces] = useState([])
+
+  useEffect(()=>{
+    axios.get('/places').then(response=>{
+      setPlaces([...response.data,...response.data,...response.data,...response.data])
+    })
+  }, [])
     return(
-    <div>
-      Index page
+    <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {places.length > 0 && places.map(place =>(
+        <div>
+          <div className=" rounded-2xl flex mb-2">
+            {place.photos?.[0] && (
+              <img className=" rounded-2xl aspect-square object-cover" src={'http://localhost:3000/uploads/'+place.photos[0]} alt="" />
+            )}
+          </div>
+          <h2 className="text-sm truncate leading-3">{place.title}</h2>
+          <h3 className="font-bold leading-4">{place.address}</h3>
+        </div>
+      ))}
     </div>
     )
 }
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -10,7 +10,7 @@ export default function PlacesPage() {
     const [places, setPlaces] = useState([])
 
     useEffect(() => {
-        axios.get('/places').then(({ data }) => {
+        axios.get('/user-places').then(({ data }) => {
             setPlaces(data)
         })
     }, [])
